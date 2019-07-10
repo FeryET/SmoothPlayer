94d53850eee1e2568be0aa41dd9825a13e64ac99
FeryET <farhoodetaati@ut.ac.ir>
2019-07-11 00:11:37 +0430

    Creates SongListAdapter and SongViewHolder.

---
+++ app/src/main/java/com/application/mvvmmusicplayer/ui/main/home/adapter/SongListAdapter.kt
@@ -0,0 +1,22 @@
+package com.application.mvvmmusicplayer.ui.main.home.adapter
+
+import android.view.ViewGroup
+import com.application.mvvmmusicplayer.models.BaseInfoModel
+import com.application.mvvmmusicplayer.models.SongModel
+import com.application.mvvmmusicplayer.ui.main.base.musicinfoviews.BaseMusicInfoListAdapter
+
+class SongListAdapter() : BaseMusicInfoListAdapter<SongModel, SongViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
