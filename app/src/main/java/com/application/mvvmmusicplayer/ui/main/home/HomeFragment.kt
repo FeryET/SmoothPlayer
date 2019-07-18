09b922aff9a28b1da3bc9799241cc505f72886e3
FeryET <farhoodetaati@ut.ac.ir>
2019-07-19 00:53:57 +0430

    Adds ButterKnife binding to HomeFragment.

--- app/src/main/java/com/application/mvvmmusicplayer/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/application/mvvmmusicplayer/ui/main/home/HomeFragment.kt
@@ -2,10 +2,13 @@ package com.application.mvvmmusicplayer.ui.main.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import butterknife.BindView
+import butterknife.ButterKnife
 import com.application.mvvmmusicplayer.R
 
 class HomeFragment: Fragment() {
@@ -16,7 +19,13 @@ class HomeFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.home_fragment_layout, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        ButterKnife.bind(view)
         super.onViewCreated(view, savedInstanceState)
     }
 
@@ -27,6 +36,4 @@ class HomeFragment: Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
     }
-
-
 }
