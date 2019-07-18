382daa868dbb9c3869bdebe3702a470870c82ae2
FeryET <farhoodetaati@ut.ac.ir>
2019-07-18 21:34:44 +0430

    Improves BaseMusicInfoListAdapter, implements SongListAdapter.

--- app/src/main/java/com/application/mvvmmusicplayer/ui/main/base/musicinfoviews/BaseMusicInfoListAdapter.kt
+++ app/src/main/java/com/application/mvvmmusicplayer/ui/main/base/musicinfoviews/BaseMusicInfoListAdapter.kt
@@ -1,19 +1,24 @@
 package com.application.mvvmmusicplayer.ui.main.base.musicinfoviews
 
+import android.content.Context
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.application.mvvmmusicplayer.models.BaseInfoModel
 
-abstract class BaseMusicInfoListAdapter<P: BaseInfoModel, T : BaseMusicInfoViewHolder> : RecyclerView.Adapter<T>() {
-    private val dataList: ArrayList<P> = ArrayList()
-    fun setData(array: ArrayList<P>){
+abstract class BaseMusicInfoListAdapter<P : BaseInfoModel, T : BaseMusicInfoViewHolder>
+    (protected val context: Context) : RecyclerView.Adapter<T>() {
+
+    protected val dataList: ArrayList<P> = ArrayList()
+    fun setData(array: ArrayList<P>) {
         dataList.clear()
         dataList.addAll(array)
     }
-    fun resetData(){
+
+    fun resetData() {
         dataList.clear()
     }
-    fun addData(item: P){
+
+    fun addData(item: P) {
         dataList.add(item)
     }
 }
--- app/src/main/java/com/application/mvvmmusicplayer/ui/main/home/adapter/SongListAdapter.kt
+++ app/src/main/java/com/application/mvvmmusicplayer/ui/main/home/adapter/SongListAdapter.kt
@@ -1,21 +1,25 @@
 package com.application.mvvmmusicplayer.ui.main.home.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.application.mvvmmusicplayer.R
 import com.application.mvvmmusicplayer.models.SongModel
 import com.application.mvvmmusicplayer.ui.main.base.musicinfoviews.BaseMusicInfoListAdapter
 
-class MusicItemListAdapter() : BaseMusicInfoListAdapter<SongModel, MusicItemViewHolder>() {
+class SongListAdapter(context: Context) : BaseMusicInfoListAdapter<SongModel, MusicItemViewHolder>(context) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicItemViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return MusicItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.music_item_view_holder, parent, false) as ViewGroup
+        )
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return dataList.size
     }
 
     override fun onBindViewHolder(holder: MusicItemViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bindData(context, dataList[position])
     }
-
 }
