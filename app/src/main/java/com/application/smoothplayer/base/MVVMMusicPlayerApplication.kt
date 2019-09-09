10755c92f777023fa9dba20228728d92d949f952
FeryET <farhoodetaati@ut.ac.ir>
2019-09-09 22:58:44 +0430

    Adds static context to application class.

--- app/src/main/java/com/application/smoothplayer/base/MVVMMusicPlayerApplication.kt
+++ app/src/main/java/com/application/smoothplayer/base/MVVMMusicPlayerApplication.kt
@@ -1,11 +1,22 @@
 package com.application.smoothplayer.base
 
+import android.annotation.SuppressLint
+import android.content.Context
 import com.application.smoothplayer.di.application.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import javax.inject.Inject
 
 class MVVMMusicPlayerApplication @Inject constructor() : DaggerApplication() {
+    companion object{
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = this
+    }
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().create(this).build()
     }
